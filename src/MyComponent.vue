9ff3ad938ceadd3f6626c39a7da442aafe020221
Eric Corson <emccorson@gmail.com>
2021-05-07 09:26:27 +0100

    Add example showing inheritAttrs not working in a mixin

---
+++ src/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="emitNativeEvent">Emit native event</button>
+</template>
+
+<script>
+const mixin = {
+  inheritAttrs: false,   // <---- this does not work
+}
+
+export default {
+  name: 'MyComponent',
+  //inheritAttrs: false,     // <---- this does work
+  mixins: [ mixin ],
+  methods: {
+    emitNativeEvent() {
+      this.$el.dispatchEvent(new Event('native-event'));
+    }
+  }
+}
+</script>
